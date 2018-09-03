7cac7b6390bee35242b7f987a5b9211af90a38f1
cryojack <wayneskatesfast@github.com>
2018-09-03 12:36:06 +0530

    Extremely late commit, some polishing done

--- formscript.js
+++ formscript.js
@@ -1,3 +1,16 @@
-function showDetails(fname, lname){
-	alert('Hello ' + fname + ' ' + lname + '!');
+var srno = 0;
+
+function addDetails(fname, lname){
+	srno = srno + 1;
+	var time = new Date();
+	var table = document.getElementById('userList');
+	var row = table.insertRow(-1);
+	var cell1 = row.insertCell(0);
+	var cell2 = row.insertCell(1);
+	var cell3 = row.insertCell(2);
+	var cell4 = row.insertCell(3);
+	cell1.innerHTML = srno;
+	cell2.innerHTML = fname;
+	cell3.innerHTML = lname;
+	cell4.innerHTML = time;
 }
--- formscript2.js
+++ formscript2.js
@@ -39,23 +39,163 @@ function changeBlue(){
 
 function changeYellow(){
 	document.getElementById('para3').style.color = 'yellow';
-<<<<<<< HEAD
 }
 
 function changeGreen(){
 	document.getElementById('para3').style.color = 'green';
 }
 
-=======
-}
-
-function changeGreen(){
-	document.getElementById('para3').style.color = 'green';
-}
-
->>>>>>> 4edc3a6a60dcb48355ad1e9a8bad7e2e55775127
 /**************************/
 
 function changeColor(r,g,b){
 	document.getElementById('para4').style.color="rgb("+r+","+g+","+b+")";
+}
+
+/*****************************
+Functions for the calculator
+*****************************/
+
+function showOutput(val){
+	if(val>=0 && val <=9){
+		document.getElementById('calcdisp').value += val;
+	}
+	else if(val == '/' || val == '*' || val == '+' || val == '-'){
+		document.getElementById('calcdisp').value += val;
+	}
+	else if(val == '.'){
+		document.getElementById('calcdisp').value += val;
+	}
+	else if(val == 'C')
+	{
+		document.getElementById('calcdisp').value="";
+	}
+	else if(val == '=')
+	{
+		var result = eval(document.getElementById('calcdisp').value);
+		document.getElementById('calcdisp').value = result;
+	}
+}
+
+/* Script for examples.html */
+
+function finalValue(userVal,val_init,val_final){
+	var result = document.getElementById('result_final');
+	var userV = userVal;
+	if (userV <= 0) {
+		result.value = "INVALID INPUT";
+	}
+	else{
+		if(val_init == 'bytes'){
+			bytes(userV,val_final);
+		}
+		if(val_init == 'kilobytes'){
+			kilobytes(userV,val_final);
+		}
+		if(val_init == 'megabytes'){
+			megabytes(userV,val_final);
+		}
+		if(val_init == 'gigabytes'){
+			gigabytes(userV,val_final);
+		}
+	}
+}
+
+function bytes(userVal,val_final){
+	var result = document.getElementById('result_final');
+	switch(val_final){
+		case 'bytes':
+		result.value = userVal;
+		break;
+
+		case 'kilobytes':
+		result.value = userVal / 1024;
+		break;
+
+		case 'megabytes':
+		result.value = (userVal / 1024) / 1024;
+		break;
+
+		case 'gigabytes':
+		result.value = ((userVal / 1024) / 1024) / 1024;
+		break;
+
+		default:
+		result.value = userVal;
+		break;
+	}
+}
+
+function kilobytes(userVal,val_final){
+	var result = document.getElementById('result_final');
+	switch(val_final){
+		case 'bytes':
+		result.value = userVal * 1024;
+		break;
+
+		case 'kilobytes':
+		result.value = userVal;
+		break;
+
+		case 'megabytes':
+		result.value = userVal / 1024;
+		break;
+
+		case 'gigabytes':
+		result.value = (userVal / 1024) / 1024;
+		break;
+
+		default:
+		result.value = userVal;
+		break;
+	}
+}
+
+function megabytes(userVal,val_final){
+	var result = document.getElementById('result_final');
+	switch(val_final){
+		case 'bytes':
+		result.value = (userVal * 1024) * 1024;
+		break;
+
+		case 'kilobytes':
+		result.value = userVal * 1024;
+		break;
+
+		case 'megabytes':
+		result.value = userVal;
+		break;
+
+		case 'gigabytes':
+		result.value = userVal * 1024;
+		break;
+
+		default:
+		result.value = userVal;
+		break;
+	}
+}
+
+function gigabytes(userVal,val_final){
+	var result = document.getElementById('result_final');
+	switch(val_final){
+		case 'bytes':
+		result.value = ((userVal * 1024) * 1024) * 1024;
+		break;
+
+		case 'kilobytes':
+		result.value = (userVal * 1024) * 1024;
+		break;
+
+		case 'megabytes':
+		result.value = (userVal * 1024);
+		break;
+
+		case 'gigabytes':
+		result.value = userVal;
+		break;
+
+		default:
+		result.value = userVal;
+		break;
+	}
 }
