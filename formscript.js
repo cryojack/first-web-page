725d8c1b07f27a0cca63270fa431b8aa018bd0c0
cryojack <wayneskatesfast@github.com>
2018-09-16 09:14:13 +0530

    another late commit, polished a few things

--- formscript.js
+++ formscript.js
@@ -1,6 +1,6 @@
 var srno = 0;
 
-function addDetails(fname, lname){
+function addDetails(fname, lname,comment){
 	srno = srno + 1;
 	var time = new Date();
 	var table = document.getElementById('userList');
@@ -9,8 +9,10 @@ function addDetails(fname, lname){
 	var cell2 = row.insertCell(1);
 	var cell3 = row.insertCell(2);
 	var cell4 = row.insertCell(3);
+	var cell5 = row.insertCell(4);
 	cell1.innerHTML = srno;
 	cell2.innerHTML = fname;
 	cell3.innerHTML = lname;
-	cell4.innerHTML = time;
+	cell4.innerHTML = comment;
+	cell5.innerHTML = time;
 }
